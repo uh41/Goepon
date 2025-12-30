e6026d1c2490b9ec9ebf5aec07ad851bc1e834f5
ike <125cr1008@tokyo.amg.ac.jp>
2025-12-30 17:43:09 +0900

    池田テストforkテスト　エフェクシアテクスチャフォルダ作成

--- Game/Game/source/modeeffekseer.cpp
+++ Game/Game/source/modeeffekseer.cpp
@@ -19,7 +19,7 @@ bool ModeEffekseer::Initialize()
 
 	// エフェクトリソースを読み込む。
 	// 読み込む時に大きさを指定する。
-	_iEffectResourceHandle = LoadEffekseerEffect("res/EF_windtry.efkefc", 100.0f);
+	_iEffectResourceHandle = LoadEffekseerEffect("res/Effect/hennsin.efkefc", 100.0f);
 
 	_vPos = VGet(1000.0f, 0.0f, 0.0f);
 
