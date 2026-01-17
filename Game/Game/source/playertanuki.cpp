6dcbbe5109093366956b94acff6cc16afd0c61e1
yuukisuzuki <125cr0007@tokyo.amg.ac.jp>
2026-01-17 16:57:24 +0900

    警告削除ができていなかったので追加

--- Game/Game/source/playertanuki.cpp
+++ Game/Game/source/playertanuki.cpp
@@ -15,7 +15,7 @@ bool PlayerTanuki::Initialize()
 {
 	if(!base::Initialize()) { return false; }
 	
-	_iHandle = MV1LoadModel("res/Tanuki/goepon_walkwalk.mv1");
+	_iHandle = MV1LoadModel("res/Tanuki/tanuponnn_01_fix.mv1");
 	_iAttachIndex = -1;
 	// ステータスを「無し」に設定
 	_status = STATUS::NONE;
@@ -116,19 +116,19 @@ bool PlayerTanuki::Process()
 	{
 		if(_iAttachIndex != -1)
 		{
-			MV1DetachAnim(_iHandle, _iAttachIndex);
+			MV1DetachAnim(_iHandle, static_cast<int>(_iAttachIndex));
 			_iAttachIndex = -1;
 		}
 		switch(_status)
 		{
 		case STATUS::WAIT:
-			_iAttachIndex = MV1AttachAnim(_iHandle, MV1GetAnimIndex(_iHandle, "walk"), -1, FALSE);
+			_iAttachIndex = static_cast<int>(MV1AttachAnim(_iHandle, MV1GetAnimIndex(_iHandle, "idle"), -1, FALSE));
 			break;
 		case STATUS::WALK:
-			_iAttachIndex = MV1AttachAnim(_iHandle, MV1GetAnimIndex(_iHandle, "walk"), -1, FALSE);
+			_iAttachIndex = static_cast<int>(MV1AttachAnim(_iHandle, MV1GetAnimIndex(_iHandle, "run"), -1, FALSE));
 			break;
 		}
-		_fTotalTime = MV1GetAttachAnimTotalTime(_iHandle, _iAttachIndex);
+		_fTotalTime = static_cast<float>(MV1GetAttachAnimTotalTime(_iHandle, _iAttachIndex));
 		_fPlayTime = 0.0f;
 		switch(_status)
 		{
@@ -166,7 +166,7 @@ bool PlayerTanuki::Render()
 	base::Render();
 
 	// 再生時間をセットする
-	MV1SetAttachAnimTime(_iHandle, _iAttachIndex, _fPlayTime);
+	MV1SetAttachAnimTime(_iHandle, _iAttachIndex, static_cast<int>(_fPlayTime));
 
 	float vorty = atan2(_vDir.x * -1, _vDir.z * -1);// モデルが標準でどちらを向いているかで式が変わる(これは-zを向いている場合)
 
@@ -180,7 +180,7 @@ bool PlayerTanuki::Render()
 
 	MATRIX m = MGetIdent();
 
-	//m = MMult(m, mRotZ);
+	m = MMult(m, mRotZ);
 	m = MMult(m, mRotY);
 	m = MMult(m, mScale);
 	m = MMult(m, mTrans);
