7feb6c36d9e89693579ed0718fa5b4762570d4a5
k-kodai01 <125cr0001@tokyo.amg.ac.jp>
2026-02-04 19:53:40 +0900

    ゲームクリアクラス

---
+++ Game/Game/source/ModeGameClear.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "modegame.h"
+#include "appframe.h"
+class ModeGameClear :public ModeGame
+{
+	typedef ModeGame base;
+public:
+	virtual bool Initialize() override;
+	virtual bool Terminate() override;
+	virtual bool Process() override;
+	virtual bool Render() override;
+
+private:
+	int _fontSize;
+};
+
