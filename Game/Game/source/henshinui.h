fe238d1a89eca83f561737377d82130131e517f8
Suzuki0007 <125cr0007@tokyo.amg.ac.jp>
2026-02-27 20:54:25 +0900

    変身選択UI追加(座標調整まだ)

--- Game/Game/source/henshinui.h
+++ Game/Game/source/henshinui.h
@@ -5,17 +5,43 @@ namespace henshin
 {
 	static constexpr auto MAKIMONO_X = 1670;
 	static constexpr auto MAKIMONO_Y = 500;
+	static constexpr auto TANUBITO_X = 1720;
+	static constexpr auto TANUBITO_Y = 500;
+	static constexpr auto TANUMONO_X = 1850;
+	static constexpr auto TANUMONO_Y = 500;
 }
 
 class HenshinUi : public UiBase
 {
 	typedef UiBase base;
 public:
+
+	enum class Select : int
+	{
+		TANUKI = 0,
+		TANUBITO = 1,
+		TANUMONO = 2,
+		Count = 3,
+		_EOT_
+	};
+
 	HenshinUi();
 	virtual ~HenshinUi() = default;
 	virtual bool Initialize();
 	virtual bool Terminate();
 	virtual bool Process();
 	virtual bool Render();
+
+	void SetOwner(void* owner) { _owner = owner; }
+
+protected:
+
+	Select _select;     // 変身UIの選択肢
+	int _handleTanubito; // タヌキ人間UIのグラフィックハンドル
+	int _handleMono;     // モノUIのグラフィックハンドル
+
+	bool _padInput5Active;	// パッド入力5Aが有効かどうか
+
+	void* _owner; // このUIを所有するオブジェクトへのポインタ
 };
 
