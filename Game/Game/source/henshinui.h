c618c7123cb2af3f5d61c33027a0e81f1018feb5
yuukisuzuki <125cr0007@tokyo.amg.ac.jp>
2026-02-28 17:26:28 +0900

    変身UI座標変更

--- Game/Game/source/henshinui.h
+++ Game/Game/source/henshinui.h
@@ -3,12 +3,12 @@
 
 namespace henshin
 {
-	static constexpr auto MAKIMONO_X = 1670;
-	static constexpr auto MAKIMONO_Y = 500;
-	static constexpr auto TANUBITO_X = 1720;
-	static constexpr auto TANUBITO_Y = 500;
-	static constexpr auto TANUMONO_X = 1850;
-	static constexpr auto TANUMONO_Y = 500;
+	static constexpr auto MAKIMONO_X = 1626;
+	static constexpr auto MAKIMONO_Y = 946;
+	static constexpr auto TANUBITO_X = 1670;
+	static constexpr auto TANUBITO_Y = 965;
+	static constexpr auto TANUMONO_X = 1800;
+	static constexpr auto TANUMONO_Y = 965;
 }
 
 class HenshinUi : public UiBase
