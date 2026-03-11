c56edf8533bc338d33d6f717fda567a5a155c453
ikedashota <125cr1008@tokyo.amg.ac.jp>
2026-03-11 18:33:21 +0900

    ゲームUI,位置調整

--- Game/Game/source/modetitle.h
+++ Game/Game/source/modetitle.h
@@ -12,7 +12,7 @@ namespace soundserver
 namespace ui
 {
 	static constexpr auto START_CONFIG_X = 0;
-	static constexpr auto START_CONFIG_Y = 1030;
+	static constexpr auto START_CONFIG_Y = 980;
 }
 
 class ModeTitle : public ModeBase
@@ -29,7 +29,7 @@ public:
 protected:
 	at::spc<TitleTanuki> _player;
 	TitleCamera* _cam;
-
+		
 	at::spc<soundserver::SoundServer> _soundServer;
 	int _gbmHandle;
 	int _startHandle;
