037dd277544fcd76576124d936de107523af8254
ichimurayoshiharu <125cr0005@tokyo.amg.ac.jp>
2026-03-11 19:22:52 +0900

    長押し宝箱の進行度を継続できるようにした

--- Game/Game/source/modegamecollision.cpp
+++ Game/Game/source/modegamecollision.cpp
@@ -640,16 +640,17 @@ bool ModeGame::CharaToTreasureOpenCollision(PlayerBase* player, const at::vspc<T
 		// 開けている宝箱が変わった場合はリセット
 		if (_currentOpeningTreasure != currentTreasure)
 		{
-			// 前の宝箱が開け途中だった場合は進行度をリセット
-			if (_currentOpeningTreasure != nullptr)
-			{
-				_treasureProgressMap[_currentOpeningTreasure] = 0.0f;
-			}
+			//// 前の宝箱が開け途中だった場合は進行度をリセット
+			//if (_currentOpeningTreasure != nullptr)
+			//{
+			//	_treasureProgressMap[_currentOpeningTreasure] = 0.0f;
+			//}
 
 			// 新しい宝箱に切り替え
 			_currentOpeningTreasure = currentTreasure;
 			_isOpeningTreasure = false;
-			_treasureHoldSec = 0.0f;
+			_treasureHoldSec = _treasureProgressMap[currentTreasure] * CHECK_OPEN_TIME;
+			//_treasureHoldSec = 0.0f;
 		}
 
 		// Aボタンを押していなかったら開けない
@@ -660,8 +661,8 @@ bool ModeGame::CharaToTreasureOpenCollision(PlayerBase* player, const at::vspc<T
 			{
 				EndCinematicCamera();
 				_isOpeningTreasure = false;
-				_treasureHoldSec = 0.0f;
-				_treasureProgressMap[currentTreasure] = 0.0f;
+				//_treasureHoldSec = 0.0f;
+				//_treasureProgressMap[currentTreasure] = 0.0f;
 				auto sound = gGlobal._soundServer->Get("60");
 				if (sound && sound->IsPlay())
 				{
@@ -669,7 +670,7 @@ bool ModeGame::CharaToTreasureOpenCollision(PlayerBase* player, const at::vspc<T
 				}
 			}
 			_isOpeningTreasure = false;
-			_treasureHoldSec = 0.0f;
+			//_treasureHoldSec = 0.0f;
 
 			// TreasureOpenUiを表示（プレイヤーの位置で）
 			if (_treasureOpenUi)
@@ -701,7 +702,7 @@ bool ModeGame::CharaToTreasureOpenCollision(PlayerBase* player, const at::vspc<T
 			{
 				TreasureOpeningCameraControl();
 				_isOpeningTreasure = true;
-				_treasureHoldSec = 0.0f;
+				//_treasureHoldSec = 0.0f;
 
 				auto sound = gGlobal._soundServer->Get("60");
 				if (sound && !sound->IsPlay())
@@ -723,7 +724,7 @@ bool ModeGame::CharaToTreasureOpenCollision(PlayerBase* player, const at::vspc<T
 			if (_treasureHoldSec >= CHECK_OPEN_TIME)
 			{
 				_treasureTakenCount++;
-				_treasureHoldSec = 0.0f;
+				//_treasureHoldSec = 0.0f;
 				currentTreasure->SetOpen(true);
 				EndCinematicCamera();
 				_isOpeningTreasure = false;
@@ -779,7 +780,7 @@ bool ModeGame::CharaToTreasureOpenCollision(PlayerBase* player, const at::vspc<T
 		{
 			EndCinematicCamera();
 			_isOpeningTreasure = false;
-			_treasureHoldSec = 0.0f;
+			//_treasureHoldSec = 0.0f;
 			// 開けていた宝箱の進行度をリセット
 			if (_currentOpeningTreasure != nullptr)
 			{
