647f6c7e861809ecdd6b4d33db50f322d5af2735
ikedashota <125cr1008@tokyo.amg.ac.jp>
2026-03-13 14:05:45 +0900

    タイトル画面用　開始、終了のアイコン、縮小版あり

--- AppFrame/source/Resources.h
+++ AppFrame/source/Resources.h
@@ -15,6 +15,7 @@ namespace img
   inline constexpr const char* UI_Makimono = "res/UI_Makimono.png";
   inline constexpr const char* UI_Tanubito = "res/UI_Tanubito.png";
   inline constexpr const char* UI_Tanumono = "res/UI_Tanumono.png";
+  inline constexpr const char* cursor = "res/cursor.png";
   inline constexpr const char* gameoverload = "res/gameoverload.png";
   inline constexpr const char* shadow_ = "res/shadow_.png";
   inline constexpr const char* split_0_0 = "res/split_0_0.png";
