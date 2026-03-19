9697928b918547bc3916f6657c35a8413e9cb94c
uhiromu <125cr1004@tokyo.amg.ac.jp>
2026-03-19 18:52:20 +0900

    ステージ1更新

--- Game/Game/source/applicationglobal.cpp
+++ Game/Game/source/applicationglobal.cpp
@@ -286,12 +286,12 @@ bool ApplicationGlobal::Init()
 	_iCgCursor = LoadGraph("res/cursor.png");
 
 	// マップ用
-	LoadMapData("Map1", "stage1_0319.json", "stage");	// 1ステージ目
+	LoadMapData("Map1", "stage1_0319x.json", "stage");	// 1ステージ目
 	LoadMapData("Map2", "stage2_0311.json", "stage");	// 2ステージ目
 	LoadMapData("Map3", "stage3_0312.json", "stage");	// 3ステージ目
 
 	//マーカー用
-	LoadStageData("Stage1", "stage1_0319.json", "stage"); // 1ステージ目
+	LoadStageData("Stage1", "stage1_0319x.json", "stage"); // 1ステージ目
 	LoadStageData("Stage2", "stage2_0319.json", "stage");     // 2ステージ目
 	LoadStageData("Stage3", "stage3_0312.json", "stage");     // 3ステージ目
 
