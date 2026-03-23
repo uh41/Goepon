de30e45258c71b3e73d54d4f6923fd10082a5666
ike <125cr1008@tokyo.amg.ac.jp>
2026-03-24 02:52:03 +0900

    キラキラエフェクト黒光り修正、開錠アクションSE調整、UI位置調整、ゲームオーバーロゴちょっと修正

--- AppFrame/source/Resources.h
+++ AppFrame/source/Resources.h
@@ -151,6 +151,7 @@ namespace mv1
   inline constexpr const char* Goal = "res/Goal/Goal.mv1";
   inline constexpr const char* makimono_01 = "res/Makimono/makimono_01.mv1";
   inline constexpr const char* SM_Stage1V2 = "res/map/SM_Stage1V2.mv1";
+  inline constexpr const char* SM_Stage3_0323 = "res/map/SM_Stage3_0323.mv1";
   inline constexpr const char* SM_stage1_0301 = "res/map/SM_stage1_0301.mv1";
   inline constexpr const char* SM_stage20303 = "res/map/SM_stage20303.mv1";
   inline constexpr const char* SM_stage3 = "res/map/SM_stage3.mv1";
